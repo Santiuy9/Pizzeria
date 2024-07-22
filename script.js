f2785c9c583a93b85bf9c521efbff18370b366b9
Santu Carballo <santu_uy@hotmail.com>
2024-07-22 02:02:55 -0300

    Version 0.3

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 let sliderInner = document.querySelector('.slider--iner');
-let images = sliderInner.querySelectorAll('img');
-let index = 1;
+if (sliderInner) {
+    let images = sliderInner.querySelectorAll('img');
+    let index = 1;
 
-setInterval(function() {
-    let percentage = index * -100;
-    sliderInner.style.transform = "translateX("+ percentage + "%)";
-    index++;
-    if(index > images.length - 1) {
-        index = 0;
-    }
-}, 5000);
+    setInterval(function() {
+        let percentage = index * -100;
+        sliderInner.style.transform = "translateX(" + percentage + "%)";
+        index++;
+        if (index > images.length - 1) {
+            index = 0;
+        }
+    }, 5000);
+}
+
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', function() {
+        // Elimina la clase 'active' de todas las tarjetas
+        document.querySelectorAll('.card').forEach(c => c.classList.remove('active'));
+
+        // Añade la clase 'active' a la tarjeta clicada
+        this.classList.add('active');
+    });
+});
