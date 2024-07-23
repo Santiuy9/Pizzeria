e0fb73ad6e50d61bd423b970ae9ba1c2d91b8f15
Santu Carballo <santu_uy@hotmail.com>
2024-07-23 00:33:12 -0300

    probando slash

---
+++ prueba.js
@@ -0,0 +1,17 @@
+
+
+
+let x = 10
+
+function leches() {
+    let x = 20;
+    if (true) {
+        let x = 30;
+        console.log(x + 10);
+    }
+    console.log(x + 10)
+}
+
+leches();
+
+console.log(x + 10);
